3d25cca179b6939fa73c3df361abeb77557a227a
Amador <guitxo@guitxo.com>
2024-10-17 07:42:55 +0200

    imagen de artista e información de popularidad en la cabecera

--- src/app/pages/spotify-api/components/artist-album-data/artist-album-data.ts
+++ src/app/pages/spotify-api/components/artist-album-data/artist-album-data.ts
@@ -1,10 +1,12 @@
 import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatExpansionModule } from '@angular/material/expansion';
 import {
   AlbumResultItem,
   ArtistResultItem,
+  ImageItemInfo,
 } from '../../../../models/spotify-api/spotify-api.model';
 
 @Component({
@@ -12,7 +14,7 @@ import {
   templateUrl: './artist-album-data.html',
   styleUrl: './artist-album-data.scss',
   standalone: true,
-  imports: [JsonPipe, NgIf, NgFor, MatExpansionModule, MatChipsModule],
+  imports: [JsonPipe, NgIf, NgFor, MatExpansionModule, MatChipsModule, MatBadgeModule],
 })
 export class ArtistAlbumDataComponent implements OnInit {
   @Input({ required: true }) isArtist!: boolean;
@@ -21,12 +23,15 @@ export class ArtistAlbumDataComponent implements OnInit {
   genres!: string[];
   followers!: number;
   popularity!: number;
+  artistImage!: ImageItemInfo | undefined;
 
   ngOnInit(): void {
     if (this.isArtist) {
       this.genres = (this.info as ArtistResultItem).genres;
       this.followers = (this.info as ArtistResultItem).followers?.total;
       this.popularity = (this.info as ArtistResultItem).popularity;
+
+      this.artistImage = this.info.images?.find(item => item.width < 320);
     }
   }
 }
