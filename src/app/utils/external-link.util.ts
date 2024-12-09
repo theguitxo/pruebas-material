08cc426ac1a55b4e53f1ce75aa38c425fe09d5ac
Amador <guitxo@guitxo.com>
2024-12-09 01:08:06 +0100

    añadido compodoc y algunos comentarios

--- src/app/utils/external-link.util.ts
+++ src/app/utils/external-link.util.ts
@@ -1,3 +1,7 @@
+/**
+ * Navega a un enlace externo de la aplicación
+ * @param {string} link url a navegar
+ */
 export function goToExternalLink(link: string): void {
   window.open(link, '_blank');
 }
